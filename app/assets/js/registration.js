94c212ba61410dd10aa992fe31259310c110de17
Dzmitry <eternal.muddle@gmail.com>
2016-10-03 04:55:55 +0000

    form validation

--- app/assets/js/registration.js
+++ app/assets/js/registration.js
@@ -23,14 +23,29 @@ app.currentModule = (function($) {
                 changeYear: true,
                 yearRange: "1920:" + new Date().getFullYear()
             });
+             
+            var otool = obj.find("label");
+
+            $(otool).tooltip({
+                    // position: {
+                    // my: "right",
+                    // at: "right+5 top-5",
+                    // collision: "none",
+                    
+                    // },
+                    track: true
+            });
+
+
+            
 
             var email = obj.find("#reglog");
             var password = obj.find("#regpass");
             var name = obj.find("#fname");
             var lastName = obj.find("#lname");
             var age = obj.find("#datepicker");
-            var gender = obj.find("#gender");
-            var agg = obj.find("#agg");
+            var gender = obj.find("input[type=radio]");
+
 
             obj.find("#registrBtn").click(function(e) {
                 e.preventDefault();
@@ -40,6 +55,17 @@ app.currentModule = (function($) {
                 var birth_date = d.split('.');
                 birth_date = new Date(birth_date[2], birth_date[1] - 1, birth_date[0]);
                 birth_date = birth_date.getTime();
+                
+                var gen_der;
+                $(gender).each(function(i, el) {
+                    if(el.checked){
+                        gen_der = el.value;
+                    }
+                });
+
+                var e_mail = /^[?0-9a-zA-Z_-]+@\w+\.\w{2,}(\.\w{2,})*$/gi.test(email.val());
+
+
 
                 function userRegistered(user) {
                     alert("User has been registered! Please log in.");
@@ -60,7 +86,7 @@ app.currentModule = (function($) {
                     try {
                         var exFields = [];
 
-                        if (!email.val()) {
+                        if (!e_mail) {
                             exFields.push(email);
                         }
 
@@ -76,8 +102,8 @@ app.currentModule = (function($) {
                         if (!age.val()) {
                             exFields.push(age);
                         }
-                        if (!gender.val()) {
-                            exFields.push(gender);
+                        if (!gen_der) {
+                            exFields.push(obj.find("#gender"));
                         }
                         if (exFields.length > 0) {
                             throw new formError(exFields);
@@ -98,7 +124,7 @@ app.currentModule = (function($) {
                         this.type = "myFormError";
                         this.styled = function() {
                             $(amir).each(function(i, el) {
-                                $(el).addClass("error");
+                                // $(el).addClass("error");
                                 var msgdiv = $("<span>Неверно заполнено поле " + $(el).attr("name") + "!</span>").css("color", "red").addClass("errMess").attr("name", '' + $(el).attr("name") + "sp" + '');
                                 $(el).after(msgdiv);
                             });
@@ -106,20 +132,26 @@ app.currentModule = (function($) {
                     }
                 }
 
+                
+
+                
+
                 var user = new Backendless.User();
                 user.email = email.val();
                 user.password = password.val();
                 user.name = name.val();
                 user.lastName = lastName.val();
                 user.age = birth_date;
-                user.gender = gender.val();
+                user.gender = gen_der;
                 user.avatar = 0;
                 user.color = "green";
+
+
 
                 Backendless.UserService.register(user, new Backendless.Async(userRegistered, gotError));
             })
             console.log();
-            obj.find('input[type=text]').each(function(i, el) {
+            obj.find('input').each(function(i, el) {
                 $(el).focusin(function() {
                     $('span[name="' + $(this).attr("name") + "sp" + '"]').remove();
                     if ($(this).hasClass("error")) {
@@ -127,6 +159,12 @@ app.currentModule = (function($) {
                     }
                 })
             })
+            // $(otool).each(function(i, el) {
+            //     $(el).focusin(function() {
+            //         console.log($(this).attr("title"));
+            //     })
+            // })
+            
         }
     }
 })(jQuery);
