aa20d273f364f51b5a0cc659c4d6ddebef62ec05
Dzmitry <eternal.muddle@gmail.com>
2016-10-07 14:01:49 +0000

    game; field.js , app.js

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -66,6 +66,7 @@ var app = (function($, cont) {
     }
 
     return {
+        
         init: function() {
             var promises = [];
 
@@ -130,7 +131,396 @@ var app = (function($, cont) {
                 $('#btnProfile, #btnField, #logIn').addClass('hide');
                 $('#formAuto').removeClass('hide');
             }
+        },
+        successLog: function(users, log){
+            console.log(users);
+            console.log(log);
+            
+            var next = log.willStart;
+            for(var key in users){
+                if(next == key){
+                next = users[key];
+            }
+            }
+            
+            // console.log(arguments);
+            // var to = data.from;
+            // var from = data.next;
+
+            //     console.log(to);
+            //     for(var key in users){
+            //         if(from == key){
+            //             from = users[key];
+            //         }
+            //     }
+            // if(log.goal){
+            //     Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+            //     var numb = log.goal.match(/[0-9]/g);
+            //     sendGame["'goal'"]
+                
+            //     backGameStorage.save()
+            // }
+            // function daTa(data){
+            //     console.log(data.data);
+            // }
+            // Backendless.enablePromises();   
+            // console.log(users);
+            // $.when(collection).then(daTa);
+            // console.log();
+            // console.log(log);
+            
+            
+            Backendless.initApp(config.id, config.key, config.v);
+            Backendless.enablePromises();
+            
+            // function currentGame(){
+            
+            
+            
+            // }
+            // function findGame(data){
+                
+            // }
+            // }
+            
+            
+            var gameStorage = Backendless.Persistence.of("game");
+            var dataQuery = new Backendless.DataQuery();
+                
+            
+            
+            function postvlog (args){
+                args = args || {};
+                this.game = users.game;
+                this.from = args.from || users.start;
+                this.to = args.to || next;
+                
+            }
+            function postgoals (args){
+                this.goals1 = args.goals1;
+                this.goals2 = args.goals2;
+                this.goals3 = args.goals3;
+                this.goals4 = args.goals4;
+            };
+            function winnewpost(args){
+                this.winner = args.winner,
+                this.ended = args.ended
+            };
+            
+            
+            function errr(err){
+                console.log( "Error message - " + err.message );
+                console.log( "Error code - " + err.statusCode );
+            }
+            function requestLog(resp){
+                
+                console.log(resp);
+                // var startGame = true;
+
+                if(resp.to == log.user1){
+                    alert("Ваш ход! Кликайте,чтобы ударить!");
+                    $("#square").on("click", function(e) {
+                        $("div[data-index]").each(function(i, el) {
+                            // console.log($(el));
+                            // console.log(e.target);
+                            // console.log(el);
+                            if(el == e.target){
+                                var index = el.getAttribute("data-index");
+                                console.log(index);
+                                var clickTo = "user"+index;
+                                console.log(clickTo);
+                                for(var key in users){
+                                    if(clickTo == key){
+                                    clickTo = users[key];
+                        
+                                    } 
+                    
+                                }console.log(clickTo);
+                                app.render(log, clickTo)
+                                otrisovka(resp.to, clickTo);
+                            }
+                        })
+                    })
+                    return false;
+                } else{
+                    app.render(log, resp.to);
+                    var to = resp.next;
+                    var from = resp.to;
+                    for(var key in users){
+                    if(to == key){
+                        to = users[key];
+                    } 
+                }
+                    otrisovka(from, to);
+                }
+
+                    function otrisovka(from, to){
+                    dataQuery.condition = "objectId = "+"'"+log.objectId+"'";
+                    gameStorage.find( dataQuery ).then(function(data){
+                    console.log(data);
+                    var game = data.data[0];
+                    var winner = game.winner;
+                    
+                    for(var key in users){
+                    if(winner == users[key]){
+                        winner = key;
+                    } 
+                    }
+                    
+                    
+                    $("#goal1").text(game.goals1);
+                    $("#goal2").text(game.goals2);
+                    $("#goal3").text(game.goals3);
+                    $("#goal4").text(game.goals4);
+                    
+                    if(resp.goal){
+                    }
+                    
+                    if(game.ended){
+                        alert(winner);
+                        return false;
+                    } else{
+                      loglog(from, to);  
+                    }
+                    });
+                    }
+
+            }
+            
+            function loglog(from, to){
+                
+                
+                
+                var postLog = new postvlog({
+                        game: log.game,
+                        from: from,
+                        to: to
+                })
+                
+                console.log(postLog);
+                
+                
+                
+                setTimeout(function(){
+                    Backendless.Persistence.of("log").save(postLog).then(requestLog).catch(errr);
+                },1000);
+                
+                 
+            }
+            loglog();
+        },
+        render: function(log, to){
+            var square = $('#square');
+            var mess = $('#info');
+
+            function Player(name, colour, avatar, id) {
+                var _self = this;
+
+                this.avatar = avatar;
+                this.name = name;
+                this.colour = colour;
+                this.id = id;
+                this.count = 0;
+                this.goal = 0;
+
+                this.punch = function() {
+                    
+                    // $(field.players.id).each(function(i, el) {
+                    //     if($(el) == from){
+                    //         _self.ball = false;
+                    //     }
+                    // })
+                    // $(field.players.id).each(function(i, el) {
+                    //     if($(el) == to){
+                    //         // field.started = false;
+                    //         // console.log(el);
+                            
+                    //     }
+                    // })
+
+                    // if (this.ball) {
+                    //     var playerId = Math.ceil(Math.random() * this.players.length) - 1;
+                    //     this.ball = false;
+
+                    //     if (Math.random() * 2 > 1) {
+                    //         field.players[playerId].addGoal();
+                    //         this.addCount();
+                    //         field.started = false;
+                    //     }
+                    //     else {
+                    //         field.players[playerId].getBall();
+                    //     }
+                    // }
+                }
+                this.ball = false;
+
+                this.getBall = function() {
+                    this.ball = true
+                };
+
+                this.addCount = function() {
+                    this.count++
+                };
+
+                this.addGoal = function() {
+                    this.goal++
+                };
+
+                this.render = function(index) {
+                    var ball = '';
+
+                    if (this.ball) {
+                        ball = '<div class="ball"></div>';
+                    }
+                    var ind = index + 1;
+                    
+                    var result = [
+                        '<div class="player ',
+                        this.colour,
+                        '" data-index="',
+                        ind,
+                        '"><img src="',
+                        this.avatar,
+                        '"><br>',
+                        this.name,
+                        ball,
+                        '</div>'
+                    ];
+
+                    return result.join('');
+                };
+
+                this.clickW = function(obj) {
+                    console.log( obj);
+                    obj = obj || this.field;
+                    console.log(this.field);
+
+                    obj.players.forEach(function(p) {
+                        p.ball = false;
+                    });
+
+                    _self.getBall();
+                    obj.render2();
+                    mess.innerHTML = 'мяч у меня!' + _self.name;
+                };
+            };
+
+            function Field(players, obj) {
+                this.players = players;
+                this.domObj = obj;
+                // this.started = false;
+
+                this.render2 = function() {
+                    var _self = this;
+                    var result = this.players.map(
+                        function(p, index) {
+                            return p.render(index);
+                        }
+                    ).join('');
+
+                    this.domObj.innerHTML = result;
+
+                    var domPlayers = document.getElementsByClassName('player');
+                    
+                    for (var i = 0; i < domPlayers.length; i++) {
+                        domPlayers[i].addEventListener('click', function(_i, _f) {
+                            return function() {
+                                _f.players[_i].clickW(_i, _f);
+                            };
+                        }(i, _self));
+                    }
+                };
+
+                this.render = function() {
+                    var _self = this;
+
+                    var result = this.players.map(
+                        function(p, index) {
+                            return p.render(index);
+                        }
+                    ).join('');
+
+                    this.domObj.html(result);
+                    
+
+                    var domPlayers = document.getElementsByClassName('player');
+                    
+                    console.log(domPlayers);
+                    
+                    // $(this.players).each(function(i, el) {
+                    //     // console.log(el);
+                    //     if(el.id == to){
+                    //         console.log(el);
+                    //         // var ev = el.click;
+                            
+                    //         field.players.forEach(function(p) {
+                    //             p.ball = false;
+                    //         });
+                    //         el.getBall();
+                    //         field.render2();
+                    //         // $(el[i]).click;
+                    //     }
+                    // })
+                    // $(field.players.id).each(function(i, el) {
+                    //     if($(el) == to){
+                    //         // field.started = false;
+                    //         console.log(el);
+                            
+                    //     }
+                    // })
+                    
+
+                    // for (var i = 0; i < domPlayers.length; i++) {
+                    //     domPlayers[i].addEventListener('click', {
+                    //         handleEvent: this.players[i].click,
+                    //         field: _self
+                    //     });
+                    // }
+                    
+                };
+
+                this.start = function() {
+                    // console.log(startGame);
+                    // if (!startGame) {
+                        // var p_Id = Math.ceil(Math.random() * this.players.length) - 1;
+                        // started = true;
+                        
+                        // this.players[startUser].getBall();
+                        $(this.players).each(function(i, el) {
+                            if(el.id == to){
+                                console.log(el)
+                                el.getBall();
+                                mess.prepend('<div>--начнём с id = "' + el.id + '</div>');
+                            }
+                        
+                        
+                        })
+                    // }
+                    // else {
+                    //     mess.prepend('<div>--Игра уже идет!</div>');
+                    // }
+                    // console.log("начнём с id = " + startUser);
+                    
+                    this.render();
+                }
+            }
+            
+            var p1 = new Player('Cap', 'green', 'assets/images/cap.png', log.user1);
+            var p2 = new Player('IronMan', 'red', 'assets/images/ironman.png', log.user2);
+            var p3 = new Player('SpiderMan', 'yellow', 'assets/images/spiderman.png', log.user3);
+            var p4 = new Player('Puh', 'blue', 'assets/images/puh.png', log.user4);
+            var pl = [p1, p2, p3, p4];
+            
+            var field = new Field([p1, p2, p3, p4], square);
+            
+            
+            field.start();
+
+            // var startGame = $('#startGame').on('click', function() {
+                
+            // });
         }
+        
     };
 
 })(jQuery, $('#app'));
--- app/assets/js/field.js
+++ app/assets/js/field.js
@@ -1,7 +1,10 @@
 app.currentModule = (function($) {
-    var gameConfig = {
-        url: 'https://api.backendless.com/v1/data/game'
-    };
+    // var gameConfig = {
+    //     url: 'https://api.backendless.com/v1/data/game'
+    // };
+        var APPLICATION_ID = 'DCE54221-04BE-2940-FF08-14B2BC0FFC00',
+        SECRET_KEY = 'E8FCB4B6-2D60-EA57-FF4B-2880988EE300',
+        VERSION = 'v1';
     
     return {
         init: function(obj, callback) {
@@ -11,55 +14,118 @@ app.currentModule = (function($) {
                 // return false;
             }
             
-            app.getAvatar();
+            // app.getAvatar();
+            
+        $(obj).find("#startGame").click(function(e){
+        var count = prompt('До скольки голов будем играть?', '5');            
 
+        function game (args){
+            args = args || {};
+            this.maxGoal = args.maxGoal || "";
+        }
+        
+        
+        Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+        Backendless.enablePromises();
+        var gameStorage = Backendless.Persistence.of("game");
+        
+        
+        function successGame(data){
+            // console.log(data);
+            var startGame = false;
+            
+            var users = {
+                user1: data.user1,
+                user2: data.user2,
+                user3: data.user3,
+                user4: data.user4,
+                game: data.objectId,
+                maxGoal: data.maxGoal,
+                ended: data.ended
+            };
+            var goals = {
+                goals1: data.goals1,
+                goals2: data.goals2,
+                goals3: data.goals3,
+                goals4: data.goals4
+            }
+
+            var start = data.willStart;
+            
+            for(var key in users){
+                if(start == key){
+                    start = users[key];
+                }
+            }
+            users.start = start;
+            var to = users.start;
+            
+            var field = $('<div id="' + gameData.objectId + '">поле ' + '</div>');
+                $('#resultFields').html('');
+                $('#resultFields').append(field);
+            app.render(data, to);
+            app.successLog(users, data);
+            
+        }
+        
+        
+        
+        function err (err){
+            console.log(err.message);
+            alert("nope");
+        }
+        
+        var gameStart = new game ({
+            maxGoal: count
+        });
+        
+        var gameData = gameStorage.save(gameStart).then(successGame).catch(err);
+        // })
             var id = Backendless.UserService.getCurrentUser().objectId;
             //показать все поля, созданные текущим игроком       
             
-            $(obj).find("#showAllFields").on('click', function(e) {
-                // console.log(app.conf);
-                $.ajax({
-                    url: gameConfig.url,
-                    method: "GET",
-                    dataType: "json",
-                    headers: {
-                        "application-id": app.conf.id,
-                        "secret-key": app.conf.key
-                    },
-                    success: getUserFields,
-                    error: function(xhRequest, ErrorText, thrownError) {
-                        console.log(xhRequest, ErrorText, thrownError);
-                    },
-                });
-            });
+            // $(obj).find("#showAllFields").on('click', function(e) {
+            //     // console.log(app.conf);
+            //     $.ajax({
+            //         url: gameConfig.url,
+            //         method: "GET",
+            //         dataType: "json",
+            //         headers: {
+            //             "application-id": app.conf.id,
+            //             "secret-key": app.conf.key
+            //         },
+            //         success: getUserFields,
+            //         error: function(xhRequest, ErrorText, thrownError) {
+            //             console.log(xhRequest, ErrorText, thrownError);
+            //         },
+            //     });
+            // });
 
-            function getUserFields(data) {
-                fields = data.data;
-                var userFields = fields.filter(function(item) {
-                    return item.ownerId === id;
-                });
+            // function getUserFields(data) {
+            //     fields = data.data;
+            //     var userFields = fields.filter(function(item) {
+            //         return item.ownerId === id;
+            //     });
 
-                userFields.forEach(function(item, i, arr) {
-                    var field = $('<div data-id="' + item.objectId + '" id="f' + i + '">поле ' + i + '</div>').on('click', function(e){ console.log(e.target) });
-                    $('#resultFields').append(field);
-                });
-            }
+            //     userFields.forEach(function(item, i, arr) {
+            //         var field = $('<div data-id="' + item.objectId + '" id="f' + i + '">поле ' + i + '</div>').on('click', function(e){ console.log(e.target) });
+            //         $('#resultFields').append(field);
+            //     });
+            // }
 
-            //создать новое поле
+            // //создать новое поле
             
-            $(obj).find("#createField").on('click', function(e) {
-                var count = prompt('До скольки голов будем играть?', '5');
+            // $(obj).find("#createField").on('click', function(e) {
+                
 
-                function game(args) {};
+            //     function game(args) {};
 
-                var contactObject = new game({
-                    maxGoal: count
-                });
+            //     var contactObject = new game({
+            //         maxGoal: count
+            //     });
 
-                var newfield = Backendless.Persistence.of(game).save(contactObject);
-                var field = $('<div id="' + newfield.objectId + '">поле ' + '</div>');
-                $('#resultFields').html('');
-                $('#resultFields').append(field);
+            //     var newfield = Backendless.Persistence.of(game).save(contactObject);
+                
             });
 
             function showNewField(data) {
@@ -69,159 +135,7 @@ app.currentModule = (function($) {
 
             }
         
-            var square = $(obj).find('#square');
-            var mess = $(obj).find('#info');
-
-            function Player(name, colour, avatar) {
-                var _self = this;
-
-                this.avatar = avatar;
-                this.name = name;
-                this.colour = colour;
-                this.count = 0;
-                this.goal = 0;
-
-                this.punch = function() {
-
-                    if (this.ball) {
-                        var playerId = Math.ceil(Math.random() * this.players.length) - 1;
-                        this.ball = false;
-
-                        if (Math.random() * 2 > 1) {
-                            field.players[playerId].addGoal();
-                            this.addCount();
-                            field.started = false;
-                        }
-                        else {
-                            field.players[playerId].getBall();
-                        }
-                    }
-                }
-                this.ball = false;
-
-                this.getBall = function() {
-                    this.ball = true
-                };
-
-                this.addCount = function() {
-                    this.count++
-                };
-
-                this.addGoal = function() {
-                    this.goal++
-                };
-
-                this.render = function(index) {
-                    var ball = '';
-
-                    if (this.ball) {
-                        ball = '<div class="ball"></div>';
-                    }
-
-                    var result = [
-                        '<div class="player ',
-                        this.colour,
-                        '" data-index="',
-                        index,
-                        '"><img src="',
-                        this.avatar,
-                        '"><br>',
-                        this.name,
-                        ball,
-                        '</div>'
-                    ];
-
-                    return result.join('');
-                };
-
-                this.click = function(e, obj) {
-                    console.log(e, obj);
-                    obj = obj || this.field;
-                    console.log(this.field);
-
-                    obj.players.forEach(function(p) {
-                        p.ball = false;
-                    });
-
-                    _self.getBall();
-                    obj.render();
-                    mess.innerHTML = 'мяч у меня!' + _self.name;
-                };
-            };
-
-            function Field(players, obj) {
-                this.players = players;
-                this.domObj = obj;
-                this.started = false;
-
-                this.render2 = function() {
-                    var _self = this;
-                    var result = this.players.map(
-                        function(p, index) {
-                            return p.render(index);
-                        }
-                    ).join('');
-
-                    this.domObj.innerHTML = result;
-
-                    var domPlayers = document.getElementsByClassName('player');
-
-                    for (var i = 0; i < domPlayers.length; i++) {
-                        domPlayers[i].addEventListener('click', function(_i, _f) {
-                            return function() {
-                                _f.players[_i].click(_i, _f);
-                            };
-                        }(i, _self));
-                    }
-                };
-
-                this.render = function() {
-                    var _self = this;
-
-                    var result = this.players.map(
-                        function(p, index) {
-                            return p.render(index);
-                        }
-                    ).join('');
-
-                    this.domObj.html(result);
-
-                    var domPlayers = document.getElementsByClassName('player');
-
-                    for (var i = 0; i < domPlayers.length; i++) {
-                        domPlayers[i].addEventListener('click', {
-                            handleEvent: this.players[i].click,
-                            field: _self
-                        });
-                    }
-                };
-
-                this.start = function() {
-                    if (!this.started) {
-                        var p_Id = Math.ceil(Math.random() * this.players.length) - 1;
-                        this.started = true;
-                        this.players[p_Id].getBall();
-                        mess.prepend('<div>--начнём с id = "' + p_Id + '</div>');
-                    }
-                    else {
-                        mess.prepend('<div>--Игра уже идет!!!!!!!!!!!!!!!!</div>');
-                    }
-                    console.log("начнём с id = " + p_Id);
-                    this.render();
-                }
-            }
-
-            var p1 = new Player('Cap', 'green', 'assets/images/cap.png');
-            var p2 = new Player('IronMan', 'red', 'assets/images/ironman.png');
-            var p3 = new Player('SpiderMan', 'yellow', 'assets/images/spiderman.png');
-            var p4 = new Player('Puh', 'blue', 'assets/images/puh.png');
-            var field = new Field([p1, p2, p3, p4], square);
-
-            var startGame = $(obj).find('#startGame');
-
-            startGame.on('click', function() {
-                field.start();
-            });
+            
 
             callback();
         }
